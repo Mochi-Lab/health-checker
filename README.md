80ae1d68f5e2950d3f2a951564b27b3ee2a1032a
dotrungkien <trungkien.keio@gmail.com>
2021-07-22 17:27:01 +0700

    init status bot

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1 align="center">
+  <br>
+      <img src="mochi.png" alt="logo" title="Mochi"  height="200" />
+  <br>
+  Mochi Market
+  <br>
+</h1>
+
+## Health checker bot
+
+## License
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+© 2021 - MochiLab
---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const Telegraf = require('telegraf')
+const session = require('telegraf/session')
+const fetch = require('node-fetch')
+
+const bot = new Telegraf(process.env.TELEGRAF_TOKEN)
+bot.use(session())
+const serverURL = process.env.SERVER_URL
+
+const healthCheck = async (ctx) => {
+  try {
+    const resp = await fetch(`${serverURL}/status`)
+    ctx.reply(`☀️☀️ server ${serverURL} alive ☀️☀️`)
+  } catch {
+    ctx.reply('☠️☠️ server dead ☠️☠️')
+  }
+}
+
+const intervalHealthCheck = async () => {
+  try {
+    const resp = await fetch(`${serverURL}/status`)
+  } catch {
+    bot.telegram.sendMessage(process.env.CHAT_ID, process.env.DEV_MENTION)
+    bot.telegram.sendMessage(
+      process.env.CHAT_ID,
+      '☠️☠️☠️☠️ server dead alert ☠️☠️☠️☠️',
+    )
+    bot.telegram.sendMessage(process.env.CHAT_ID, `${serverURL}`)
+  }
+}
+
+setInterval(() => {
+  intervalHealthCheck()
+}, 60000)
+
+bot.start(async (ctx) => {
+  healthCheck(ctx)
+})
+
+bot.command('status', async (ctx) => {
+  healthCheck(ctx)
+})
+
+bot.startPolling() //MUST HAVE
