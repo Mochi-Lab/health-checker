678e72e4478e7ed355a89acc75277c3d0ef3063b
Do Trung Kien <trungkien.keio@gmail.com>
2021-07-23 14:30:41 +0700

    update status command

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-require('dotenv').config()
-const Telegraf = require('telegraf')
-const session = require('telegraf/session')
-const fetch = require('node-fetch')
+require('dotenv').config();
+const Telegraf = require('telegraf');
+const session = require('telegraf/session');
+const fetch = require('node-fetch');
 
-const bot = new Telegraf(process.env.TELEGRAF_TOKEN)
-bot.use(session())
-const serverURL = process.env.SERVER_URL
+const bot = new Telegraf(process.env.TELEGRAF_TOKEN);
+bot.use(session());
+const serverURL = process.env.SERVER_URL;
 
 const healthCheck = async (ctx) => {
   try {
-    const resp = await fetch(`${serverURL}/status`)
-    ctx.reply(`☀️☀️ server ${serverURL} alive ☀️☀️`)
+    const resp = await fetch(`${serverURL}/status`);
+    ctx.reply(`☀️☀️ server ${serverURL} alive ☀️☀️`);
   } catch {
-    ctx.reply('☠️☠️ server dead ☠️☠️')
+    ctx.reply('☠️☠️ server dead ☠️☠️');
   }
-}
+};
 
 const intervalHealthCheck = async () => {
   try {
-    const resp = await fetch(`${serverURL}/status`)
+    const resp = await fetch(`${serverURL}/status`);
+    if (resp.status !== 200) {
+      bot.telegram.sendMessage(process.env.CHAT_ID, process.env.DEV_MENTION);
+      bot.telegram.sendMessage(process.env.CHAT_ID, '☠️☠️☠️☠️ server dead alert ☠️☠️☠️☠️');
+      bot.telegram.sendMessage(process.env.CHAT_ID, `${serverURL}`);
+    }
   } catch {
-    bot.telegram.sendMessage(process.env.CHAT_ID, process.env.DEV_MENTION)
-    bot.telegram.sendMessage(
-      process.env.CHAT_ID,
-      '☠️☠️☠️☠️ server dead alert ☠️☠️☠️☠️',
-    )
-    bot.telegram.sendMessage(process.env.CHAT_ID, `${serverURL}`)
+    bot.telegram.sendMessage(process.env.CHAT_ID, process.env.DEV_MENTION);
+    bot.telegram.sendMessage(process.env.CHAT_ID, '☠️☠️☠️☠️ server dead alert ☠️☠️☠️☠️');
+    bot.telegram.sendMessage(process.env.CHAT_ID, `${serverURL}`);
   }
-}
+};
 
 setInterval(() => {
-  intervalHealthCheck()
-}, 60000)
+  intervalHealthCheck();
+}, 60000);
 
 bot.start(async (ctx) => {
-  healthCheck(ctx)
-})
+  healthCheck(ctx);
+});
 
 bot.command('mochistatus', async (ctx) => {
-  healthCheck(ctx)
-})
+  healthCheck(ctx);
+});
 
-bot.startPolling() //MUST HAVE
+bot.startPolling(); //MUST HAVE
